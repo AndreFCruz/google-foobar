de413b19e1c56c6851b4e215adf64e57dd7642a9
AndreFCruz <andrecruz97@gmail.com>
2022-03-05 16:42:32 +0000

    new level 3 problem

--- level-3_bomb-baby/solution.py
+++ level-3_bomb-baby/solution.py
@@ -1,18 +1,36 @@
 
 def solution(M, F):
+    """
+    Each bomb can generate one of the other type of bomb, so
+    they're **interchangeable**.
+
+    Start from the end-point (number of bombs required), and
+    work backwards until reaching the starting point of (1, 1).
+
+    Find the bomb with largest gap between target and current
+    (M > F ? M : F), and produce (remove from target, working
+    backwords).
+    """
     M, F = int(M), int(F)
 
+    # Bomb types are interchangeable
     if M < F:
         M, F = F, M
 
+    # Use M for the largest number and always decrement M in
+    # each iteration;
+    # If M is no longer the largest number, switch M,F;
+
     cnt = 0
     while M != F:
+        # Replicate F bombs cnt number of times,
+        # to produce cnt*F bombs of type M;
         cnt += M // F
         M = M % F
         if M < F:
             M, F = F, M
         
-        if min(M, F) <= 0:
+        if M <= 0 or F <= 0:
             break
 
     return str(cnt-1) if M == 1 else "impossible"
