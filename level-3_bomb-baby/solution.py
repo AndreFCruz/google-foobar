462b481b4f6ca51f5cedecde10e8476338d21032
AndreFCruz <andrecruz97@gmail.com>
2022-03-05 16:23:36 +0000

    level-3 bombs baby solution

--- level-3_bomb-baby/solution.py
+++ level-3_bomb-baby/solution.py
@@ -1,2 +1,29 @@
-def solution(x, y):
-    # Your code here
+
+def solution(M, F):
+    M, F = int(M), int(F)
+
+    if M < F:
+        M, F = F, M
+
+    cnt = 0
+    while M != F:
+        cnt += M // F
+        M = M % F
+        if M < F:
+            M, F = F, M
+        
+        if min(M, F) <= 0:
+            break
+
+    return str(cnt-1) if M == 1 else "impossible"
+
+
+if __name__ == "__main__":
+    print(solution("4", "7"))
+    # Output: 4
+
+    print(solution("2", "1"))
+    # Output: 1
+
+    print(solution("2", "4"))
+    # Output: impossible
